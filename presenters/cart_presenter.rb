097854fe2e2668eca13059383bca9316edb88517
Washington Botelho <wbotelhos@gmail.com>
2023-09-02 19:33:14 -0300

    ft: adds cart presenter

---
+++ presenters/cart_presenter.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class CartPresenter
+  def initialize(data)
+    @data = data
+  end
+
+  def line_items
+    data.map do |item|
+      product = item[:product]
+
+      [
+        item[:quantity],
+        product.imported? ? 'imported' : nil,
+        "#{product.name}:",
+        format_number(item[:total]),
+      ].compact.join(' ')
+    end
+  end
+
+  def taxes
+    format_number(data.sum { |item| item[:taxes] })
+  end
+
+  def total
+    format_number(data.sum { |item| item[:total] })
+  end
+
+  private
+
+  attr_reader :data
+
+  def format_number(number)
+    format("%.2f", number)
+  end
+end
---
+++ spec/presenter/cart_presenter_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'pry-byebug'
+require 'rspec'
+
+require_relative '../../models/product'
+require_relative '../../presenters/cart_presenter'
+
+RSpec.describe CartPresenter do
+  describe '#line_items' do
+    it "returns the line items formatted" do
+      data = [
+        { product: Product.new(imported: true, name: 'one'), quantity: 1, total: 1.11 },
+        { product: Product.new(imported: false, name: 'two'), quantity: 2, total: 2.22 },
+        { product: Product.new(imported: true, name: 'three'), quantity: 3, total: 3.33 },
+      ]
+
+      expect(CartPresenter.new(data).line_items).to eq(
+        [
+          "1 imported one: 1.11",
+          "2 two: 2.22",
+          "3 imported three: 3.33",
+        ]
+      )
+    end
+  end
+
+  describe '#taxes' do
+    it "returns the sum of all products taxes" do
+      data = [{ taxes: 1.11 }, { taxes: 2.22 }, { taxes: 3.33 }]
+
+      expect(CartPresenter.new(data).taxes).to eq('6.66')
+    end
+  end
+
+  describe '#total' do
+    it "returns the sum of all products total" do
+      data = [{ total: 1.11 }, { total: 2.22 }, { total: 3.33 }]
+
+      expect(CartPresenter.new(data).total).to eq('6.66')
+    end
+  end
+end
